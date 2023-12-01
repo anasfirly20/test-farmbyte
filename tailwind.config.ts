c0ab573d48405a5081a125e5aec318baa450eca1
Anas Firly <anasfirly20@gmail.com>
2023-12-01 13:00:00 +0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,74 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "sky-blue": "#4582b4",
+      },
+      padding: {
+        shorter4: "1vw",
+        shorter3: "2vw",
+        shorter2: "3vw",
+        shorter: "4vw",
+        normal: "6vw",
+        longer: "8vw",
+        longer2: "10vw",
+        longer3: "12vw",
+        longer4: "14vw",
+        longer5: "16vw",
+        longer6: "18vw",
+        longer7: "20vw",
+        longer8: "22vw",
+        longer9: "24vw",
+        longer10: "26vw",
+      },
+      margin: {
+        shorter4: "1vw",
+        shorter3: "2vw",
+        shorter2: "3vw",
+        shorter: "4vw",
+        normal: "6vw",
+        longer: "8vw",
+        longer2: "10vw",
+        longer3: "12vw",
+        longer4: "14vw",
+        longer5: "16vw",
+        longer6: "18vw",
+        longer7: "20vw",
+        longer8: "22vw",
+        longer9: "24vw",
+        longer10: "26vw",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#BEF264",
+              foreground: "#000000",
+            },
+            focus: "#BEF264",
+          },
+        },
+      },
+    }),
+  ],
+};
+export default config;
