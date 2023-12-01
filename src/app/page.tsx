b2dca8e61f32bf6169fab40300eee6c0970b76e3
Anas Firly <anasfirly20@gmail.com>
2023-12-02 00:45:24 +0700

    feat: integrating the api icon code with its svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,27 @@ import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 
 // Assets
-import snowy from "../../public/animated/snowy-3.svg";
+// Day
+import day1 from "../../public/animated/01d.svg";
+import day2 from "../../public/animated/02d.svg";
+import day3 from "../../public/animated/03d.svg";
+import day4 from "../../public/animated/04d.svg";
+import day5 from "../../public/animated/09d.svg";
+import day6 from "../../public/animated/10d.svg";
+import day7 from "../../public/animated/11d.svg";
+import day8 from "../../public/animated/13d.svg";
+import day9 from "../../public/animated/50d.svg";
+
+// Night
+import night1 from "../../public/animated/01n.svg";
+import night2 from "../../public/animated/02n.svg";
+import night3 from "../../public/animated/03n.svg";
+import night4 from "../../public/animated/04n.svg";
+import night5 from "../../public/animated/09n.svg";
+import night6 from "../../public/animated/10n.svg";
+import night7 from "../../public/animated/11n.svg";
+import night8 from "../../public/animated/13n.svg";
+import night9 from "../../public/animated/50n.svg";
 
 // Components
 import Empty from "@/components/Empty";
@@ -67,6 +87,38 @@ export default function Home() {
     }
   };
 
+  type TIconMapping = {
+    [key: string]: string;
+  };
+
+  const getWeatherIcon = (iconCode: string) => {
+    const weatherIconMappings: TIconMapping = {
+      // Day
+      "01d": day1,
+      "02d": day2,
+      "03d": day3,
+      "04d": day4,
+      "09d": day5,
+      "10d": day6,
+      "11d": day7,
+      "13d": day8,
+      "50d": day9,
+
+      // Night
+      "01n": night1,
+      "02n": night2,
+      "03n": night3,
+      "04n": night4,
+      "09n": night5,
+      "10n": night6,
+      "11n": night7,
+      "13n": night8,
+      "50n": night9,
+    };
+
+    return weatherIconMappings[iconCode] || day1;
+  };
+
   return (
     <section className="py-longer px-longer h-[95vh]">
       <section className="flex items-center justify-between flex-wrap gap-y-3">
@@ -98,15 +150,20 @@ export default function Home() {
         <>
           <section>
             <div className="flex items-center mt-6">
-              <Image src={snowy} alt="snowy" width={120} height={120} />
+              <Image
+                src={
+                  dataCurrentWeather?.weather?.[0]?.icon &&
+                  getWeatherIcon(dataCurrentWeather?.weather?.[0]?.icon)
+                }
+                alt="snowy"
+                width={120}
+                height={120}
+              />
               <h1 className="text-sky-600">{dataCurrentWeather?.name}</h1>
             </div>
             <p className="text-gray-400">
               {dataCurrentWeather?.dt &&
-                moment
-                  .unix(dataCurrentWeather?.dt)
-                  .format("dddd, D MMMM YYYY, HH:mm")}
-              {/* {dataCurrentWeather?.dt && moment.unix(dataCurrentWeather?.dt).format("MMMM D, YYYY")} */}
+                moment.unix(dataCurrentWeather?.dt).format("dddd, D MMMM YYYY")}
             </p>
             <h2 className="flex items-start text-sky-600 mt-4">
               {dataCurrentWeather?.main?.temp &&
@@ -137,8 +194,11 @@ export default function Home() {
                   <h5>{data?.dt && moment.unix(data?.dt).format("HH:mm")}</h5>
                   <Image
                     priority
-                    src={snowy}
-                    alt="snowy"
+                    src={
+                      data?.weather?.[0]?.icon &&
+                      getWeatherIcon(data?.weather?.[0]?.icon)
+                    }
+                    alt="Weather's icon"
                     width={60}
                     height={60}
                   />
