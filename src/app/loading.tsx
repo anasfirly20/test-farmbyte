c2bfa4e2ed1b60cc289061b5c57ff45ad0d3898e
Anas Firly <anasfirly20@gmail.com>
2023-12-02 11:54:24 +0700

    style: adjusting height for loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@nextui-org/spinner";
 
 export default function loading() {
   return (
-    <section className="h-[70vh] flex justify-center items-center">
+    <section className="h-[95vh] flex justify-center items-center">
       <Spinner color="primary" size="lg" label="Loading..." />
     </section>
   );
