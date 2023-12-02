1d99d07994dda630866daf8d35c5f001bfed7e14
Anas Firly <anasfirly20@gmail.com>
2023-12-02 10:33:53 +0700

    style: adjusting footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <a
-      href="https://github.com/anasfirly20/test-farmbyte"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex justify-center pb-4"
-    >
-      <Image src={githubSvg} alt="github mark" width={30} height={30} />
-    </a>
+    <footer className="flex justify-center pb-4">
+      <a
+        href="https://github.com/anasfirly20/test-farmbyte"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-fit"
+      >
+        <Image src={githubSvg} alt="github mark" width={30} height={30} />
+      </a>
+    </footer>
   );
 }
